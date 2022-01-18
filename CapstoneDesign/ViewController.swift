8e66d1b62d5909d9327a3511e1f38f12b6b70191
oh_jun <vhzkclq0705@naver.com>
2022-01-18 15:51:59 +0900

    Initial Commit

---
+++ CapstoneDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapstoneDesign
+//
+//  Created by 권오준 on 2022/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
